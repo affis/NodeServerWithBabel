04cae7aa83d1e6912dc9634b0defc805fae3001f
Alfred <alfred.kroll@gmail.com>
2017-06-05 18:14:40 +0200

    Add array to JSON function with word counter

--- index.js
+++ index.js
@@ -14,13 +14,31 @@ function removeEmptyStrings(data)
     return cleanData;
 }
 
+function transformToJSON(data)
+{
+  let jsonLyrics = {}
+  for(let i = 0; i<data.length ;i++)
+  {
+    let word = data[i].toLowerCase();
+    if(jsonLyrics.hasOwnProperty(word))
+    {
+      jsonLyrics[word] += 1;
+    }
+    else {
+      jsonLyrics[word] = 1;
+    }
+  }
+  return jsonLyrics;
+}
+
 fs.readFile('./dummy.txt', 'utf8', function(err, contents){
     let dataRead = contents.replace(/(\[.*?\])|\(|\)/g,"").split(/\s/g);
     let lyrics = removeEmptyStrings(dataRead);
-    console.log(lyrics);
+    console.log(transformToJSON(lyrics));
     // console.log(lyrics[30]);
 })
-//http.get()
+
+// let lyrcisJson = JSON.stringify(lyrics);
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
